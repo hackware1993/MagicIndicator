88bce667157ba2969d2a700424943cc9e25a95e1
hackware1993 <cfb1993@163.com>
2016-09-08 23:25:17 +0800

    ColorTransitionPagerTitleView去掉interpolator

--- app/src/main/java/net/lucode/hackware/magicindicatordemo/ext/titles/ScaleTransitionPagerTitleView.java
+++ app/src/main/java/net/lucode/hackware/magicindicatordemo/ext/titles/ScaleTransitionPagerTitleView.java
@@ -17,17 +17,17 @@ public class ScaleTransitionPagerTitleView extends ColorTransitionPagerTitleView
     }
 
     @Override
-    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
-        super.onLeave(index, totalCount, leavePercent, leftToRight);    // 实现颜色渐变
-        setScaleX(1.0f + (mMinScale - 1.0f) * getEndInterpolator().getInterpolation(leavePercent));
-        setScaleY(1.0f + (mMinScale - 1.0f) * getEndInterpolator().getInterpolation(leavePercent));
+    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
+        super.onEnter(index, totalCount, enterPercent, leftToRight);    // 实现颜色渐变
+        setScaleX(mMinScale + (1.0f - mMinScale) * enterPercent);
+        setScaleY(mMinScale + (1.0f - mMinScale) * enterPercent);
     }
 
     @Override
-    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
-        super.onEnter(index, totalCount, enterPercent, leftToRight);    // 实现颜色渐变
-        setScaleX(mMinScale + (1.0f - mMinScale) * getStartInterpolator().getInterpolation(enterPercent));
-        setScaleY(mMinScale + (1.0f - mMinScale) * getStartInterpolator().getInterpolation(enterPercent));
+    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
+        super.onLeave(index, totalCount, leavePercent, leftToRight);    // 实现颜色渐变
+        setScaleX(1.0f + (mMinScale - 1.0f) * leavePercent);
+        setScaleY(1.0f + (mMinScale - 1.0f) * leavePercent);
     }
 
     public float getMinScale() {
--- magicindicator/src/main/java/net/lucode/hackware/magicindicator/buildins/commonnavigator/titles/ColorTransitionPagerTitleView.java
+++ magicindicator/src/main/java/net/lucode/hackware/magicindicator/buildins/commonnavigator/titles/ColorTransitionPagerTitleView.java
@@ -1,8 +1,6 @@
 package net.lucode.hackware.magicindicator.buildins.commonnavigator.titles;
 
 import android.content.Context;
-import android.view.animation.Interpolator;
-import android.view.animation.LinearInterpolator;
 
 import net.lucode.hackware.magicindicator.buildins.ArgbEvaluatorHolder;
 
@@ -13,11 +11,21 @@ import net.lucode.hackware.magicindicator.buildins.ArgbEvaluatorHolder;
  * Created by hackware on 2016/6/26.
  */
 public class ColorTransitionPagerTitleView extends SimplePagerTitleView {
-    private Interpolator mStartInterpolator = new LinearInterpolator();
-    private Interpolator mEndInterpolator = new LinearInterpolator();
 
     public ColorTransitionPagerTitleView(Context context) {
         super(context);
+    }
+
+    @Override
+    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
+        int color = ArgbEvaluatorHolder.eval(leavePercent, mSelectedColor, mNormalColor);
+        setTextColor(color);
+    }
+
+    @Override
+    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
+        int color = ArgbEvaluatorHolder.eval(enterPercent, mNormalColor, mSelectedColor);
+        setTextColor(color);
     }
 
     @Override
@@ -26,39 +34,5 @@ public class ColorTransitionPagerTitleView extends SimplePagerTitleView {
 
     @Override
     public void onDeselected(int index, int totalCount) {
-    }
-
-    @Override
-    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
-        int color = ArgbEvaluatorHolder.eval(mEndInterpolator.getInterpolation(leavePercent), getSelectedColor(), getNormalColor());
-        setTextColor(color);
-    }
-
-    @Override
-    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
-        int color = ArgbEvaluatorHolder.eval(mStartInterpolator.getInterpolation(enterPercent), getNormalColor(), getSelectedColor());
-        setTextColor(color);
-    }
-
-    public Interpolator getStartInterpolator() {
-        return mStartInterpolator;
-    }
-
-    public void setStartInterpolator(Interpolator startInterpolator) {
-        mStartInterpolator = startInterpolator;
-        if (mStartInterpolator == null) {
-            mStartInterpolator = new LinearInterpolator();
-        }
-    }
-
-    public Interpolator getEndInterpolator() {
-        return mEndInterpolator;
-    }
-
-    public void setEndInterpolator(Interpolator endInterpolator) {
-        mEndInterpolator = endInterpolator;
-        if (mEndInterpolator == null) {
-            mEndInterpolator = new LinearInterpolator();
-        }
     }
 }
