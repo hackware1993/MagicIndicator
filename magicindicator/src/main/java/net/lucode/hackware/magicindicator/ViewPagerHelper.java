00af853bfb9598d27a5fae3074acb33ce5da2a02
wangshaolei <wangshaolei@dofun.cn>
2021-12-27 09:48:13 +0800

    rename: bind viewpager2

--- magicindicator/src/main/java/net/lucode/hackware/magicindicator/ViewPagerHelper.java
+++ magicindicator/src/main/java/net/lucode/hackware/magicindicator/ViewPagerHelper.java
@@ -30,23 +30,20 @@ public class ViewPagerHelper {
         });
     }
 
-    public static void bindViewPager2(final MagicIndicator magicIndicator, ViewPager2 viewPager2) {
+    public static void bind(final MagicIndicator magicIndicator, ViewPager2 viewPager2) {
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
                 magicIndicator.onPageScrolled(position, positionOffset, positionOffsetPixels);
             }
 
             @Override
             public void onPageSelected(int position) {
-                super.onPageSelected(position);
                 magicIndicator.onPageSelected(position);
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
                 magicIndicator.onPageScrollStateChanged(state);
             }
         });
